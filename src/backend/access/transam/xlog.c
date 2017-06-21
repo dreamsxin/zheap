10ccdce79fed7a4dd42321198c8f8a1e9bd26dda
Thomas Munro <thomas.munro@enterprisedb.com>
2017-06-21 13:59:31 +1200

    Implemented UndoLogDiscard and related things.
    Now able to discard and recycle segment files on master and standby.
    Assorted other changes: using an LWLock per undo log, instead of a SpinLock.
    Using base/undo rather than base/9 to hold undo segment files.  Renamed
    segment files as logno.offset, so that they also tell you the UndoRecPtr
    of the first byte in the segment.  Got rid of the separate tracking of
    'mvcc' and 'rollback' discard pointers; I don't think that's my job, instead
    there is just a single 'discard' pointer.  Renamed 'capacity' to 'end'.
    
    There is plenty more work to be done here...

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -9891,6 +9891,9 @@ xlog_redo(XLogReaderState *record)
 		XLogCtl->ckptXid = checkPoint.nextXid;
 		SpinLockRelease(&XLogCtl->info_lck);
 
+		/* Write an undo log metadata snapshot. */
+		CheckPointUndoLogs(checkPoint.redo);
+
 		/*
 		 * We should've already switched to the new TLI before replaying this
 		 * record.
@@ -9949,6 +9952,9 @@ xlog_redo(XLogReaderState *record)
 		XLogCtl->ckptXidEpoch = checkPoint.nextXidEpoch;
 		XLogCtl->ckptXid = checkPoint.nextXid;
 		SpinLockRelease(&XLogCtl->info_lck);
+
+		/* Write an undo log metadata snapshot. */
+		CheckPointUndoLogs(checkPoint.redo);
 
 		/* TLI should not change in an on-line checkpoint */
 		if (checkPoint.ThisTimeLineID != ThisTimeLineID)
