60731dbac9433dc640d3412504423e15260b1ed6
Mithun CY <mithun.cy@gmail.com>
2018-04-25 23:52:55 -0700

    Fix rebase issue
    Prototype change of GetOldestXmin was not updated while rebasing to
    postgres main branch. Above patch corrects same.

--- src/backend/catalog/index.c
+++ src/backend/catalog/index.c
@@ -3030,7 +3030,7 @@ IndexBuildZHeapRangeScan(Relation zheapRelation,
 		else
 		{
 			snapshot = SnapshotAny;
-			OldestXmin = GetOldestXmin(zheapRelation, true);
+			OldestXmin = GetOldestXmin(zheapRelation, PROCARRAY_FLAGS_VACUUM);
 		}
 
 		scan = zheap_beginscan_strat(zheapRelation,		/* relation */
