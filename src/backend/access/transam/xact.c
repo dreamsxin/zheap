304dfa7af0547db1f22d72b30b42baa92647b136
dilip kumar <dilip.kumar@enterprisedb.com>
2018-06-27 20:49:04 -0700

    Fix compilation warning

--- src/backend/access/transam/xact.c
+++ src/backend/access/transam/xact.c
@@ -3207,7 +3207,6 @@ void
 XactPerformUndoActionsIfPending()
 {
 	TransactionState s = CurrentTransactionState;
-	TransactionId xid = s->transactionId;
 	uint64 rollback_size = 0;
 	bool new_xact = true, result = false;
 	UndoRecPtr parent_latest_urec_ptr;
@@ -3256,7 +3255,6 @@ XactPerformUndoActionsIfPending()
 			 * into this transaction's state so that if there is some error while
 			 * performing undo actions we can restart from begining.
 			 */
-			xid = xact->transactionId;
 			xact->start_urec_ptr = UndoActionEndPtr;
 			xact->latest_urec_ptr = UndoActionStartPtr;
 		}
